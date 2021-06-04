b63afe21b0c98a463f81f48b3d19b73a77703b3b
DESKTOP-T80V890\Syed Kabir S <kabir.syed@studytonight.com>
2021-06-04 10:29:30 +0530

    testing in main class

--- src/com/inheritanceTrainScenario/FareDetailsMain.java
+++ src/com/inheritanceTrainScenario/FareDetailsMain.java
@@ -3,6 +3,7 @@ package com.inheritanceTrainScenario;
 public class FareDetailsMain {
     public static void main(String[] args){
         NonAcTicket nonAcTicket = new NonAcTicket("Mayiladuturai","chennai", 1200, "DFR34","12/06/21",350);
-        nonAcTicket.getTatkalFare();
+        System.out.println("for booking non-Ac coach in tatkal, the fare is: " + nonAcTicket.getTatkalFare());
+        System.out.println("reservation cost for non-Ac coach is " + nonAcTicket.getReservationPrice());
     }
 }
